d2814a3a29cbdf613001a9f88cf30a359c02ad14
Szilard Magyar <szilard.magyar@gmail.com>
2017-06-13 22:55:40 +0200

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   mount ImageUploader::UploadEndpoint => "/images/upload"
   devise_for :users
 
-  authenticated :user do
-    root to: 'projects#index', as: :authenticated_root
-  end
   root to: 'users#index'
 
   resources :users, only: [:show] do
